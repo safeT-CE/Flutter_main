cced802cf5d6fc557f8441de8d481eed3ab07478
jungeunkim <grace0130kim@gmail.com>
2024-08-05 22:10:51 +0900

    🚀map api key added again

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
-import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:provider/provider.dart';
 import 'package:safet/pages/agreement_page.dart';
 import 'package:safet/pages/auth_done_page.dart';
+import 'package:safet/pages/auth_face_cam_page.dart';
 import 'package:safet/pages/auth_phonenum_page.dart';
 
 import 'pages/agreement_page.dart';
 import 'pages/alarm_page.dart';
 import 'pages/announcement_page.dart';
+import 'pages/ask_page.dart';
 import 'pages/auth_done_page.dart';
 import 'pages/auth_face_cam_page.dart';
 import 'pages/auth_face_how_page.dart';
@@ -19,25 +22,27 @@ import 'pages/auth_page.dart';
 import 'pages/auth_phonenum_page.dart';
 import 'pages/auth_phonenum_verificate_page.dart';
 import 'pages/home_page.dart';
+import 'pages/identification_page.dart';
+import 'pages/inquiry_data.dart';
 import 'pages/lock_page.dart';
 import 'pages/login_page.dart';
 import 'pages/map_page.dart';
+import 'pages/number_input_page.dart';
+import 'pages/one_on_one_inquiry_page.dart';
+import 'pages/payment_selection_page.dart';
+import 'pages/penalty_page.dart';
 import 'pages/profile_page.dart';
 import 'pages/rent_page.dart';
 import 'pages/return_page.dart';
 import 'pages/splash_page.dart';
-import 'pages/payment_page.dart';
-import 'pages/number_input_page.dart';
-import 'pages/qr_scanner_page.dart';
-import 'pages/identification_page.dart';
-
+import 'pages/theme_provider.dart';
+import 'pages/violation_data.dart';
 import '../utils/auth_helper.dart';
 
-//색상 선언
-const Color safeTblack=Color(0xFF1A1A1A);
-const Color safeTgray=Color(0xFFA1A1A1);
-const Color safeTgreen=Color(0xFFAFD08F);
-const Color safeTlightgreen=Color(0xFFE7F1DE);
+const Color safeTblack = Color(0xFF1A1A1A);
+const Color safeTgray = Color(0xFFA1A1A1);
+const Color safeTgreen = Color(0xFFAFD08F);
+const Color safeTlightgreen = Color(0xFFE7F1DE);
 
 Future<void> main() async {
   AuthRepository.initialize(appKey: '0fb225459d4b8c516f20ff340eceb313');
@@ -55,39 +60,46 @@ class MyApp extends StatelessWidget {
   final CameraDescription frontCamera;
   final CameraDescription backCamera;
 
-  MyApp({required this.frontCamera, required this.backCamera});
+  const MyApp({required this.frontCamera, required this.backCamera});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SafeT',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => InquiryData()),
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (context) => ViolationData()),
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            title: 'SafeT',
+            theme: themeProvider.isDarkMode ? ThemeData.dark() : ThemeData.light(),
+            home: SplashPage(),
+            routes: {
+              '/agreement': (context) => AgreementPage(),
+              '/alarm': (context) => AlarmPage(),
+              '/announcement': (context) => AnnouncementPage(),
+              '/auth': (context) => AuthPage(),
+              '/home': (context) => HomePage(),
+              '/login': (context) => LoginPage(),
+              '/map': (context) => MapPage(),
+              '/profile': (context) => ProfilePage(),
+              '/rent': (context) => RentPage(),
+              '/return': (context) => ReturnPage(),
+              '/payment': (context) => PaymentSelectionPage(),
+              '/number': (context) => NumberInputPage(onNumberEntered: () {}),
+              '/identification': (context) => IdentificationPage(),
+              '/ask': (context) => AskPage(),
+              '/penalty': (context) => PenaltyPage(),
+              '/one_on_one_inquiry': (context) => OneOnOneInquiryPage(
+                initialCategory: '벌점',
+                initialTitle: '벌점 기록에 대한 문의',
+              ),
+            },
+          );
+        },
       ),
-      home: SplashPage(),
-      routes: {
-        '/agreement': (context) => AgreementPage(),
-        '/alarm': (context) => AlarmPage(),
-        '/announcement': (context) => AnnouncementPage(),
-        '/auth_done': (context) => AuthDonePage(),
-        '/auth_face_cam': (context) => FaceCamPage(camera: frontCamera),
-        '/auth_face_how': (context) => FaceHowPage(),
-        '/auth_id_cam': (context) => IdCamPage(camera: backCamera),
-        '/auth_id_how': (context) => IdHowPage(),
-        '/auth_idinfo': (context) => IdInfoCheckPage(recognizedLines: []),
-        '/auth': (context) => AuthPage(),
-        '/auth_phonenumber': (context) => PhoneNumberInputPage(),
-        '/home': (context) => HomePage(),
-        '/login': (context) => LoginPage(),
-        '/map': (context) => MapPage(),
-        '/profile': (context) => ProfilePage(),
-        '/rent': (context) => RentPage(),
-        '/return': (context) => ReturnPage(),
-        '/payment': (context) => PaymentPage(),
-        '/qr': (context) => QrScannerPage(),
-        '/number': (context) => NumberInputPage(),
-        '/identification': (context) => IdentificationPage(),
-      },
     );
   }
 }
