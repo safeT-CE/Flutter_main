1a079f9a25bf514a65b3cbe395830d6fdeef51e7
minyoung24 <dlalsdudWkd@gmail.com>
2024-08-14 21:56:41 +0900

    상세이용

--- lib/pages/identification_page.dart
+++ lib/pages/identification_page.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:safet/main.dart';
 
 class IdentificationPage extends StatefulWidget {
   @override
@@ -45,7 +46,20 @@ class _IdentificationPageState extends State<IdentificationPage> {
               children: <Widget>[
                 Expanded(
                   flex: 4,
-                  child: CameraPreview(_controller),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      CameraPreview(_controller),
+                      Container(
+                        width: 250,
+                        height: 250,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.red, width: 2),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
                 Expanded(
                   flex: 2,
@@ -54,16 +68,26 @@ class _IdentificationPageState extends State<IdentificationPage> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
-                          '계정 소유자와 동일인인지 판별하는 과정입니다.\n얼굴을 원 안에 넣어 주세요.',
+                          '얼굴을 영역 안에 맞추고\n촬영해 주세요.',
                           textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                        SizedBox(height: 16),
+                        SizedBox(height: 30),
                         ElevatedButton(
                           onPressed: () {
                             // 여기에서 동일인 판별 로직을 추가할 수 있습니다.
                             Navigator.pop(context, true); // 동일인 판별이 성공하면 true를 반환
                           },
                           child: Text('다음'),
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white, 
+                            backgroundColor: safeTgreen, // 텍스트 색상
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 20),
+                          ),
                         ),
                       ],
                     ),
